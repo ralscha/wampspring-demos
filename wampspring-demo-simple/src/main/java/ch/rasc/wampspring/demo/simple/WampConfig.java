94e4acc9aa44c39070777156ed4f3148c1e4650a
Ralph Schaer <ralphschaer@gmail.com>
2015-01-27 13:11:02 +0100

    Fix demos

--- wampspring-demo-simple/src/main/java/ch/rasc/wampspring/demo/simple/WampConfig.java
+++ wampspring-demo-simple/src/main/java/ch/rasc/wampspring/demo/simple/WampConfig.java
@@ -1,26 +1,13 @@
 package ch.rasc.wampspring.demo.simple;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import ch.rasc.wampspring.config.DefaultWampConfiguration;
 import ch.rasc.wampspring.config.WampEndpointRegistry;
 import ch.rasc.wampspring.cra.AuthenticationSecretProvider;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @Configuration
 public class WampConfig extends DefaultWampConfiguration {
-
-	// Reusing the objectMapper that spring boot provides
-
-	@Autowired
-	ObjectMapper objectMapper;
-
-	@Override
-	public ObjectMapper objectMapper() {
-		return this.objectMapper;
-	}
 
 	@Override
 	public void registerWampEndpoints(WampEndpointRegistry registry) {
--- wampspring-demo-various/src/main/java/ch/rasc/wampspring/demo/various/WampConfig.java
+++ wampspring-demo-various/src/main/java/ch/rasc/wampspring/demo/various/WampConfig.java
@@ -2,13 +2,13 @@ package ch.rasc.wampspring.demo.various;
 
 import org.springframework.context.annotation.Configuration;
 
+import ch.rasc.wampspring.config.AbstractWampConfigurer;
 import ch.rasc.wampspring.config.EnableWamp;
-import ch.rasc.wampspring.config.WampConfigurerAdapter;
 import ch.rasc.wampspring.config.WampEndpointRegistry;
 
 @Configuration
 @EnableWamp
-public class WampConfig extends WampConfigurerAdapter {
+public class WampConfig extends AbstractWampConfigurer {
 
 	@Override
 	public void registerWampEndpoints(WampEndpointRegistry registry) {
