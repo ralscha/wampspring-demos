ca75db8d1653b976a032c592ae338a734ccef902
Ralph Schaer <ralphschaer@gmail.com>
2015-01-29 18:48:57 +0100

    Make a war package. We deploy it to a tomcat

--- wampspring-demo-security/README.md
+++ wampspring-demo-security/README.md
@@ -1,3 +1,7 @@
+Online: https://demo.rasc.ch/wampspring-demo-security/
+
+Port of the [websocket sample application](https://github.com/spring-projects/spring-session/tree/master/samples/websocket) from [Spring Session](http://projects.spring.io/spring-session/)
+
 Login with
 rob
 luke
--- wampspring-demo-security/src/main/java/ch/rasc/wampspring/demo/security/Application.java
+++ wampspring-demo-security/src/main/java/ch/rasc/wampspring/demo/security/Application.java
@@ -2,11 +2,16 @@ package ch.rasc.wampspring.demo.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-@EnableScheduling
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
