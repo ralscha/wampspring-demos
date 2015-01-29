1b815faa83ba1c000210fc6cd4bbd7204f169310
Ralph Schaer <ralphschaer@gmail.com>
2015-01-29 19:30:58 +0100

    Make a war package. We deploy it to a tomcat

--- wampspring-demo-various/src/main/java/ch/rasc/wampspring/demo/various/Application.java
+++ wampspring-demo-various/src/main/java/ch/rasc/wampspring/demo/various/Application.java
@@ -2,13 +2,21 @@ package ch.rasc.wampspring.demo.various;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
 }
